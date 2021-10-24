e6406c4482c097b6d32e310387f4ebbdcf4ea6bc
Risi Kondor <risi@cs.uchicago.edu>
2021-10-24 03:00:28 -0500

    'changes'

---
+++ pytorch/cnine_py.cpp
@@ -0,0 +1,8 @@
+#include <torch/torch.h>
+#include <pybind11/stl.h>
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+
+
+
+}
---
+++ pytorch/setup.py
@@ -0,0 +1,26 @@
+import sys,os
+import torch 
+from setuptools import setup
+from torch.utils.cpp_extension import CppExtension, BuildExtension, CUDAExtension
+
+os.environ['CUDA_HOME']='/usr/local/cuda'
+os.environ["CC"] = "clang"
+
+CUDA_HOME='/usr/local/cuda'
+
+print(torch.cuda.is_available())
+
+setup(name='cnine',
+ext_modules=[CUDAExtension('cnine', ['cnine_py.cpp'],
+                                 include_dirs=['/usr/local/cuda/include',
+                                               '../include','../objects/scalar','../objects/tensor'],
+                                 library_dirs=['/usr/local/cuda/lib'],
+                                 runtime_libraries=['cudart'],
+                                 libraries=['cudart','dl'],
+                                 extra_compile_args = {'nvcc': ['-D_WITH_CUDA'],
+                                                       'cxx': ['-Wno-sign-compare','-Wno-deprecated-declarations',
+                                                               '-D_WITH_CUDA']},
+                                 depends=['setup.py'])], 
+      cmdclass={'build_ext': BuildExtension}
+      )
+
