d564123e8d6f98001adeac196a37683d392bb083
Risi Kondor <risi@cs.uchicago.edu>
2021-11-26 23:22:54 -0600

    'changes'

--- python/torch_cnine.py
+++ python/torch_cnine.py
@@ -1,15 +1,55 @@
 import torch
 import cnine
+from cnine import rtensor as _rtensor
 
 
 class rtensor(torch.Tensor):
 
     @staticmethod
-    def zeros(i0,i1):
-        return rtensor(torch.zeros(i0,i1))
+    def zeros(*args):
+        return rtensor(torch.zeros(*args))
     
-    #def __init__(self,x):
-     #   super().__assign__(self,x) #.__init__(x)
-    #def __init__(self,i0,i1):
-     #   print('kljj2')
-      #  self=torch.zeros(i0,i1)
+    @staticmethod
+    def ones(*args):
+        return rtensor(torch.ones(*args))
+    
+    @staticmethod
+    def eye(*args):
+        return rtensor(torch.eye(*args))
+    
+    @staticmethod
+    def randn(*args):
+        return rtensor(torch.randn(*args))
+
+    def __str__(self):
+        return _rtensor.view(self).__str__()
+
+    def __repr__(self):
+        return _rtensor.view(self).__repr__()
+
+
+
+class ctensor(torch.Tensor):
+
+    @staticmethod
+    def zeros(*args):
+        return ctensor(torch.zeros((2,)+args))
+    
+    @staticmethod
+    def ones(*args):
+        return ctensor(torch.ones((2,)+args))
+    
+    @staticmethod
+    def eye(*args):
+        return ctensor(torch.eye((2,)+args))
+    
+    @staticmethod
+    def randn(*args):
+        return ctensor(torch.randn((2,)+args))
+    
+    def __str__(self):
+        return _ctensor.view(self).__str__()
+
+    def __repr__(self):
+        return _ctensor.view(self).__repr__()
+
